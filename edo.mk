00d04491d38e48a226e5d9bd6a62c10df91d7a60
spectredev-007 <mohammad.galaxy07@gmail.com>
2022-11-08 08:55:46 +0000

    sm8250-common: switch to aidl power hal
    Change-Id: I0328eab207a8a373f31f355bfbf09cc6f9b3539b

--- edo.mk
+++ edo.mk
@@ -423,9 +423,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service.sony-libperfmgr
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -453,7 +451,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/sony
+    hardware/sony \
+    hardware/google \
+    hardware/google/interfaces
 
 # Shim
 PRODUCT_PACKAGES += \
