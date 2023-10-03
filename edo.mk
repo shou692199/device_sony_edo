b4bd179dec1d643dcd0f0a84cab24b0d302b6065
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-10-04 03:15:41 +0530

    sm8250-common: treewide: Drop duplicate entries
    Change-Id: I39bae525920a8b8f2c7fc7d714b038f23f97e7be
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- edo.mk
+++ edo.mk
@@ -258,10 +258,6 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
-    libqdMetaData \
-    libtinyxml \
-    libvulkan \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
@@ -273,6 +269,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libsdmcore \
     libsdmutils \
+    libtinyxml \
+    libvulkan \
     memtrack.kona \
     vendor.display.config@1.5 \
     vendor.display.config@1.11.vendor \
