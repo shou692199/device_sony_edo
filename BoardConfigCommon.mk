e4431a008e79e510271ddf122bdfdaea37269120
shou692199 <shou692199@gmail.com>
2023-11-27 22:08:03 +0800

    edo: Build charger HAL

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,6 +212,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include hardware/sony/XperiaModules/XperiaCharger/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
