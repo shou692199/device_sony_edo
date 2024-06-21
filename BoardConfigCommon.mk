4ef38023de7ec4f2b891e16b22e2466fb8977c29
蕭澧邦 <shou692199@gmail.com>
2024-06-21 22:40:18 +0800

    edo: fix sepolicy include path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -209,7 +209,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-10-01
 
 # SEPolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/sony/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
