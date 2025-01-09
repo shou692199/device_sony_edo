4a169abb3794d9864dd6577026154b338b1d6715
LuK1337 <priv.luk@gmail.com>
2025-01-09 12:57:53 +0100

    sm8250-common: Build gralloc.qcom
    Camera HAL wants it and crashes if it loads default gralloc
    implementation instead.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8059
    Change-Id: I47965713c822cfdfc369975f8975f21f70133608

--- edo.mk
+++ edo.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    gralloc.qcom \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
