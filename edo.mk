b0cad0789f3025de86ede0f1a96ce643adcf0d36
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sm8250-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- edo.mk
+++ edo.mk
@@ -381,6 +381,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.sony-libperfmgr
