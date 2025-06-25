1d3e061ec4a480e6cb1ea5974554faee5bbfe1aa
夜香竹筍 <shou692199@gmail.com>
2025-06-25 15:08:48 +0800

    edo: Switch to common sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -204,8 +204,6 @@ VENDOR_SECURITY_PATCH := 2022-10-01
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/sony/sepolicy/qti/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
